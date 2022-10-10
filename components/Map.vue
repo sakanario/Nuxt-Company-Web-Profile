a72f307950f0f7e70a06c098ed9c8a49556cb687
Satria L <satrialaksana12@gmail.com>
2022-10-10 13:03:58 +0700

    integrasi map

--- components/Map.vue
+++ components/Map.vue
@@ -3,7 +3,7 @@
     <iframe
       class="w-100"
       id="gmap_canvas"
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15839.508847879848!2d107.53869249999998!3d-7.0237165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68ec32bba247ad%3A0x67a5f639a74fa907!2sGedung%20Budaya%20Sabilulungan!5e0!3m2!1sen!2sid!4v1664941111778!5m2!1sen!2sid"
+      :src="ContactData?.map"
       frameborder="0"
       scrolling="no"
       marginheight="0"
@@ -34,3 +34,15 @@
     </style>
   </div>
 </template>
+
+
+<script>
+export default {
+  name: "Map",
+  computed: {
+    ContactData() {
+      return this.$store.state.contact.contactData;
+    },
+  },
+};
+</script>
