bd1263daa7916c6c5aed2cd6ebebe1eb100e5fd5
Satria L <satrialaksana12@gmail.com>
2022-10-12 23:10:58 +0700

    add smooth loading transition

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
-    <div v-if="this.$fetchState.pending">
-      <Loading/>
-    </div>
-    <div v-else>
+    <transition name="loading">
+      <div v-if="this.$fetchState.pending">
+        <Loading />
+      </div>
+    </transition>
+
+    <div
+      class="transition-opacity duration-700 ease-in"
+      :class="{
+        'opacity-0': this.$fetchState.pending,
+        'opacity-100': !this.$fetchState.pending,
+      }"
+    >
       <Navbar />
       <Nuxt />
       <Footer />
@@ -12,22 +21,27 @@
 </template>
 
 <script>
-import Navbar  from '@/components/layout/Navbar.vue'
-import Footer  from '@/components/layout/Footer.vue'
+import Navbar from "@/components/layout/Navbar.vue";
+import Footer from "@/components/layout/Footer.vue";
 
 export default {
-  components:{
+  components: {
     Navbar,
-    Footer
+    Footer,
   },
   async fetch() {
-    await this.$store.dispatch('fetchCompanyData')
-    await this.$store.dispatch('service/fetchServiceData')
-    await this.$store.dispatch('customer/fetchCustomerData')
-    await this.$store.dispatch('product/fetchProductData')
-    await this.$store.dispatch('contact/fetchContactData')
+    await this.$store.dispatch("fetchCompanyData");
+    await this.$store.dispatch("service/fetchServiceData");
+    await this.$store.dispatch("customer/fetchCustomerData");
+    await this.$store.dispatch("product/fetchProductData");
+    await this.$store.dispatch("contact/fetchContactData");
   },
   fetchOnServer: false,
-}
+  transition: "loading",
+};
 </script>
 
+<style scoped>
+  .loading-enter-active, .loading-leave-active { transition: opacity 0.5s; }
+  .loading-enter, .loading-leave-active { opacity: 0; }
+</style>
