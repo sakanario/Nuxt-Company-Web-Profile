f80a92a85a90cbd8b73ef3e6f85ee17a7e4c5f6b
Satria L <satrialaksana12@gmail.com>
2022-10-15 00:22:23 +0700

    about done

--- components/About.vue
+++ components/About.vue
@@ -11,7 +11,7 @@
             v-html="CompanyData.title"
             class="text-4xl lg:text-6xl font-semibold py-3"
           ></h1>
-          <p v-html="CompanyData.sub_title" class="description lg:text-2xl"></p>
+          <p v-html="CompanyData.sub_title" class="description"></p>
         </div>
       </div>
 
@@ -23,37 +23,43 @@
 
     <!-- Transition 1 -->
     <div id="transition" class="relative">
-      <!-- <img
-        id="transition-image-1"
-        class="w-full absolute"
-        src="../assets/img/substrat-1.svg"
-      /> -->
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#335E89" fill-opacity="1" d="M0,32L80,69.3C160,107,320,181,480,176C640,171,800,85,960,53.3C1120,21,1280,43,1360,53.3L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 190">
+        <path
+          fill="#335E89"
+          fill-opacity="1"
+          d="M0,32L80,69.3C160,107,320,181,480,176C640,171,800,85,960,53.3C1120,21,1280,43,1360,53.3L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
+        ></path>
+      </svg>
     </div>
 
     <!-- Second Section -->
-    <div id="secondAbout"
-      class="section-2 p-5 sm:p-12 sm:py-24 text-white flex flex-wrap sm:flex-row-reverse sm:flex-row md:flex-nowrap md:justify-center"
+    <div
+      id="secondAbout"
+      class="section-2 p-5 py-12 lg:py-24 flex flex-wrap sm:flex-row-reverse sm:flex-row md:flex-nowrap justify-center"
     >
       <!-- Left -->
-      <div class="flex max-w-2xl sm:w-3/4 sm:ml-12">
+      <div class="flex max-w-2xl  md:ml-12">
         <div class="my-auto">
-          <p v-html="CompanyData.company_description" class="md:text-2xl"></p>
+          <p v-html="CompanyData.company_description" class="description"></p>
+          <!-- <p class="description md:text-2xl">
+            membuat kami mampu meningkatkan produktivitas, efisensi dan
+            signifikasi dalam peforma setiap perusahaan rekan bisnis kami.
+          </p> -->
         </div>
       </div>
 
       <!-- Right -->
-      <div class="mx-10 sm:mx-0 w-full flex justify-center sm:w-1/4">
+      <div class="mx-10 sm:mx-0 w-full flex justify-center md:w-1/4">
         <img class="w-full py-5" src="../assets/img/draw-2.svg" />
       </div>
     </div>
     <!-- Transition 1 -->
     <div id="transition" class="relative" style="width: 100%">
-      <img
+      <!-- <img
         id="transition-image-2"
         class="w-full absolute"
         src="../assets/img/substrat-2.svg"
-      />
+      /> -->
     </div>
   </div>
 </template>
@@ -63,8 +69,8 @@ export default {
   name: "About",
   computed: {
     CompanyData() {
-      return this.$store.state.companyData
-    }
-  }
+      return this.$store.state.companyData;
+    },
+  },
 };
 </script>
